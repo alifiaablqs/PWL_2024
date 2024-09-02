0f052019934752adeaa49c4d9ba73d59aa6a6f65
alifiaablqs <dyalifiabalqis22@gmail.com>
2024-09-02 10:20:47 +0700

    Jobsheet2-Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () { 
+    return 'World';
+});
+
+Route::get('/welcome', function () { 
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () { 
+    return 'Nama: Dyalifia Balqis Susanto <br>NIM: 2241760085 ';
+});
+
+Route::get('/user/fia', function ($name = 'Dyalifia Balqis Susanto') { 
+    return 'Nama Saya: ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+Route::get('/articles/{post}/comments/{comment}', function ($commentId) {
+    return 'Halaman Artikel ini '."dengan ID ".$commentId;
+    });
+        
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
+    });
+
+
+    
+        
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +48,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
