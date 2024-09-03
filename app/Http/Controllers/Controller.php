a786bfa89623107b1c30fb0d1814acb5e03aff89
alifiaablqs <dyalifiabalqis22@gmail.com>
2024-09-03 20:01:22 +0700

    Jobsheet2-Praktikum2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,16 +12,14 @@ class Controller extends BaseController
     use AuthorizesRequests, ValidatesRequests;
 }
 
-namespace App\Http\Controllers; 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-class WelcomeController extends Controller
-{
-public function hello() {
-    return 'Hello World';
-    }
-}
+
+
+
+
+
+
+
 
 
 
