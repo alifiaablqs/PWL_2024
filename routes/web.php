8bcaf14fa2ed2e1173fecf75addf29b74a75ed60
alifiaablqs <dyalifiabalqis22@gmail.com>
2024-09-03 20:01:45 +0700

    Jobsheet2-Praktikum2

--- routes/web.php
+++ routes/web.php
@@ -1,69 +1,48 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
 
 
-Route::get('/world', function () { 
+Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/welcome', function () { 
-    return 'Selamat Datang';
-});
 
-Route::get('/about', function () { 
-    return 'Nama: Dyalifia Balqis Susanto <br>NIM: 2241760085 ';
-});
-
-Route::get('/user/fia', function ($name = 'Dyalifia Balqis Susanto') { 
-    return 'Nama Saya: ' . $name;
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
 });
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-    });
-    
-Route::get('/articles/{post}/comments/{comment}', function ($commentId) {
-    return 'Halaman Artikel ini '."dengan ID ".$commentId;
-    });
-        
-Route::get('/user/{name?}', function ($name='John') { 
-    return 'Nama saya '.$name;
-    });
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
 
 
-use App\Http\Controllers\PhotoController;
-Route::resource('photos', PhotoController::class);
-    
-Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
-]);
-
-Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
-]);
-
-Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Dyalifia Balqis Susanto']);
-    });
-
-Route::get('/greeting', function () {        
-    return view('blog.hello', ['name' => 'Dyalifia Balqis Susanto']);
-    });
-        
-    
-        
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
+Route::get('/user/{name?}', function ($name = "John") {
+    return 'Nama saya ' . $name;
+});
